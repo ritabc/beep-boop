ddad926e9cf6ec0ebcd1d35bff3e48f82398212d
ritabc <ritabc@gmail.com>
2018-06-08 10:54:06 -0700

    Update lookForZero function to lookForZeroOrOne, add isDivisibleByThree function

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,36 @@ function pushBoop(output) {
   return output
 }
 
-function lookForZero(number, output) {
+function pushApology(output, name) {
+  output.push("I'm sorry, " + name + ". I'm afraid I can't do that.")
+  return output
+}
+
+function lookForZeroOrOne(number, output) {
   arrayOfDigits = number.toString().split("")
   // arrayOfDigits = ["1", "4"]
   // loop and check for any O's
   for (let i = 0; i < arrayOfDigits.length; ++i) {
-    if (parseInt(arrayOfDigits[i]) === 0) {
-      pushBeep(output);
-      return output
+    // if (parseInt(arrayOfDigits[i]) % 3 === 0) {
+    //   console.log("reached")
+    //   pushApology(output, name)
+    //   break
+    if (parseInt(arrayOfDigits[i]) === 1) {
+      pushBoop(output)
+      break
+    } else if (parseInt(arrayOfDigits[i]) === 0) {
+      pushBeep(output)
+      break
     }
   }
+  return output
 }
 
-lookForZero(20, [])
+// lookForZeroOrOne(20, [])
+
+function isDivisibleByThree(number, output, name) {
+  if (number % 3 === 0) {
+    pushApology(output, name)
+    return output
+  }
+}
